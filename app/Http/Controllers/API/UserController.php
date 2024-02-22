b2e249ad6caec5d4a95d111ca5d3ad5cdd548a96
JOYMALAKAR7 <malakarjoy4@gmail.com>
2024-02-22 14:22:20 +0530

    User registration with validation and post request

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,12 +5,27 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     //Function to create User
     function createUser(Request $request)
     {
+        $validator=Validator::make($request->all(),[
+            'name'=>"required|string",
+            'email'=>"required|string",
+            'phone'=>"required|numeric",
+            'password'=>"required|min:6"
+          
+        ]);
+        if($validator->fails()){
+            $resul = array('status' => false, 'message' => "something went wrong",
+            'error_message'=>$validator->error());
+            return response()->json($resul,400);
+        }
+
+      
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
